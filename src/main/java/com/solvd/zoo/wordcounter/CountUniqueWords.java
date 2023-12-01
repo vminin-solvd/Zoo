fbce00a00452917c34d564933d28bf5fddb52196
Victor <vminin@ncsu.edu>
2023-12-01 14:43:06 -0500

    Changed Main.java to use try catch blocks for all exceptions.

--- src/main/java/com/solvd/zoo/wordcounter/CountUniqueWords.java
+++ src/main/java/com/solvd/zoo/wordcounter/CountUniqueWords.java
@@ -1,10 +1,7 @@
 package com.solvd.zoo.wordcounter;
 
-import com.solvd.zoo.person.Visitor;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -13,12 +10,9 @@ import java.util.Map;
 
 public class CountUniqueWords {
 
-    private static final Logger LOGGER = LogManager.getLogger(Visitor.class);
+    public static void countUniqueWord(String fileName) throws IOException{
 
-    public static void countUniqueWord() {
-
-        try {
-            File file = new File(getResourcePath("FamousPhrases.txt"));
+            File file = new File(getResourcePath(fileName));
             String fileContent = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
             String[] words = fileContent.split("\\s+");
             Map<String, Integer> wordCountMap = new HashMap<>();
@@ -34,10 +28,8 @@ public class CountUniqueWords {
             int uniqueWords = wordCountMap.size();
             String newFileContent = fileContent + " " + uniqueWords;
             FileUtils.writeStringToFile(file, newFileContent, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            LOGGER.error("Error occurred while processing the file", e);
         }
-    }
+
     private static String getResourcePath(String resourceName) {
         return CountUniqueWords.class.getClassLoader().getResource(resourceName).getPath();
     }
